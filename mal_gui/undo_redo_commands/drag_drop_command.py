d2828c0cba49a17be13a011de45567b2ea8e4fa7
Joakim Loxdal <loxdal@kth.se>
2025-02-19 12:09:15 +0100

    Make sure assets and attackers are removed when undoing drag/drop of them

--- mal_gui/undo_redo_commands/drag_drop_command.py
+++ mal_gui/undo_redo_commands/drag_drop_command.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from PySide6.QtGui import QUndoCommand
+from ..object_explorer import AssetItem, AttackerItem
 
 if TYPE_CHECKING:
     from mal_gui.model_scene import ModelScene
@@ -14,6 +15,7 @@ class DragDropCommand(QUndoCommand):
 
     def redo(self):
         """Perform drag and drop"""
+        print("REDO DROP!")
         self.scene.addItem(self.item)
 
         #Update the Object Explorer when number of items change
@@ -22,6 +24,12 @@ class DragDropCommand(QUndoCommand):
     def undo(self):
         """Undo drag and drop"""
         self.scene.removeItem(self.item)
+        print("UNDO DROP!")
+
+        if isinstance(self.item, AssetItem):
+            self.scene.model.remove_asset(self.item.asset)
+        elif isinstance(self.item, AttackerItem):
+            self.scene.model.remove_attacker(self.item.attacker)
 
         #Update the Object Explorer when number of items change
         self.scene.main_window.update_childs_in_object_explorer_signal.emit()
